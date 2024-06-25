7173d1946a93314a680c25c54cf4475fb3bb2507
Usman Khan <usman918khan@gmail.com>
2024-06-25 16:55:44 +0500

    mediumLeetCode

---
+++ Problem Solving Practive/Medium.js
@@ -0,0 +1,18 @@
+// Problem 1
+
+
+var numPairsDivisibleBy60 = function (time) {
+    let count = 0
+    for (let i = 0; i < time.length; i++) {
+        for (let j = i + 1; j < time.length; j++) {
+            if ((time[i] + time[j]) % 60 === 0){
+                count++
+            }
+            
+        }
+
+    }
+    return count
+};
+console.log(numPairsDivisibleBy60([30, 20, 150, 100, 40]))
+console.log(20 % 60)
