d70590836991c409be5c7fad9259607b42aa2197
Usman Khan <usman918khan@gmail.com>
2024-06-25 21:20:24 +0500

    leetcode

--- Problem Solving Practive/Medium.js
+++ Problem Solving Practive/Medium.js
@@ -1,6 +1,6 @@
 // Problem 1
 
-
+/*
 var numPairsDivisibleBy60 = function (time) {
     let count = 0
     for (let i = 0; i < time.length; i++) {
@@ -16,3 +16,32 @@ var numPairsDivisibleBy60 = function (time) {
 };
 console.log(numPairsDivisibleBy60([30, 20, 150, 100, 40]))
 console.log(20 % 60)
+*/
+
+var setZeroes = function(matrix) {
+    let rows = new Set();
+    let cols = new Set();
+
+    // First pass to find all zeros
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 0) {
+                rows.add(i);
+                cols.add(j);
+            }
+        }
+    }
+
+    // Second pass to set zeros
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (rows.has(i) || cols.has(j)) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    return matrix;
+};
+
+console.log(setZeroes([[1, 1, 1], [1, 0, 1], [1, 1, 1]]))
