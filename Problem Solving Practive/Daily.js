1527acf1783f1f7df4e2873ccadb777c00f36ab8
Usman Khan <usman918khan@gmail.com>
2024-07-02 11:08:23 +0500

    dailyChallenge

---
+++ Problem Solving Practive/Daily.js
@@ -0,0 +1,19 @@
+//      02/07/2024
+
+
+var intersect = function(nums1, nums2) {
+    let arr = []
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            if(nums1[i] === nums2[j]){
+                arr.push(nums1[i])
+                nums2.splice(j, 1)
+                //nums1.splice(i, 1)
+                break;
+            }
+            
+        }
+    }
+    return arr
+};
+console.log(intersect([4,9,5], [9,4,9,8,4]))
