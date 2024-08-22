60a9943d3293aa28bdecb309877d350465906fcd
Usman Khan <usman918khan@gmail.com>
2024-08-22 20:21:56 +0500

    leetcode

--- Problem Solving Practive/Medium.js
+++ Problem Solving Practive/Medium.js
@@ -293,3 +293,12 @@ var longestIdealString = function (s, k) {
 
 console.log(longestIdealString("pvjcci", 4)); // Example output
 
+var findComplement = function(num) {
+    if (num === 0) return 1;
+
+const bitLength = num.toString(2).length;
+
+const mask = (1 << bitLength) - 1;
+
+return num ^ mask;
+};
