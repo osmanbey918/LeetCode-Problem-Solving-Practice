3923c4609822ed99790cf6e4fa5e0a2349a919c9
Usman Khan <usman918khan@gmail.com>
2024-08-28 22:56:15 +0500

    leetcode

--- Problem Solving Practive/Hard.js
+++ Problem Solving Practive/Hard.js
@@ -23,19 +23,19 @@ var minKBitFlips = function(nums, k) {
     return flipCount;
 };*/
 
-var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
+// var search = function(nums, target) {
+//     let left = 0;
+//     let right = nums.length - 1;
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        if (nums[mid] === target) {
-            return mid;
-        } else if (nums[mid] > target) {
-            right = mid - 1;
-        } else {
-            left = mid + 1;
-        }
-    }
-    return -1;
-};
+//     while (left <= right) {
+//         let mid = Math.floor((left + right) / 2);
+//         if (nums[mid] === target) {
+//             return mid;
+//         } else if (nums[mid] > target) {
+//             right = mid - 1;
+//         } else {
+//             left = mid + 1;
+//         }
+//     }
+//     return -1;
+// };
