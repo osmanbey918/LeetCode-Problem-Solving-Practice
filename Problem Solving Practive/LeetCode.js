5f1066b9db2a8af6436dad25468047d1fa06df94
Usman Khan <usman918khan@gmail.com>
2024-08-29 20:52:51 +0500

    leetcode

--- Problem Solving Practive/LeetCode.js
+++ Problem Solving Practive/LeetCode.js
@@ -567,11 +567,11 @@ var findMaxAverage = function(nums, k) {
 };
 
 
-var numDifferentIntegers = function(word) {
-    word = word.replaceAll(/[a-z]/g, " ").split(" ");
- const nums = new Set();
- for(const w of word) {
-     if(w !== "" && !nums.has(BigInt(w))) nums.add(BigInt(w));
- }
- return nums.size;
-};
+// var numDifferentIntegers = function(word) {
+//     word = word.replaceAll(/[a-z]/g, " ").split(" ");
+//  const nums = new Set();
+//  for(const w of word) {
+//      if(w !== "" && !nums.has(BigInt(w))) nums.add(BigInt(w));
+//  }
+//  return nums.size;
+// };
