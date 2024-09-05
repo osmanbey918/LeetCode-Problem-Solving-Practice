6ec8118b1015ae86b92ca6dc2d177ea61e2370a5
Usman Khan <usman918khan@gmail.com>
2024-09-05 19:45:14 +0500

    leetcode

--- Problem Solving Practive/LeetCode.js
+++ Problem Solving Practive/LeetCode.js
@@ -632,3 +632,25 @@ let max = Math.max(...numArr);
 let min = Math.min(...numArr);
 
 console.log(max);
+
+var missingRolls = function(rolls, mean, n) {
+    let m = rolls.length;
+   let totalSum = mean * (n + m);
+   let observedSum = rolls.reduce((a, b) => a + b, 0);
+   
+   let missingSum = totalSum - observedSum;
+   
+   if (missingSum < n || missingSum > 6 * n) {
+       return [];
+   }
+   
+   let base = Math.floor(missingSum / n);
+   let remainder = missingSum % n;
+   
+   let result = new Array(n).fill(base);
+   for (let i = 0; i < remainder; i++) {
+       result[i] += 1;
+   }
+   
+   return result;
+};
