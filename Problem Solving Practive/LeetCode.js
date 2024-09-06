0a8068c1116637fb67f70e1b0d9a9ebbd939df58
Usman Khan <usman918khan@gmail.com>
2024-09-06 14:56:56 +0500

    leetcode

--- Problem Solving Practive/LeetCode.js
+++ Problem Solving Practive/LeetCode.js
@@ -654,3 +654,28 @@ var missingRolls = function(rolls, mean, n) {
    
    return result;
 };
+
+var modifiedList = function (nums, head) {
+    let max = -1;
+    for (let num of nums) {
+        max = num > max ? num : max;
+    }
+
+    let freq = new Array(max + 1).fill(false);
+
+    for (let num of nums) freq[num] = true;
+
+    let temp = new ListNode();
+    let current = temp;
+
+    while (head != null) {
+        if (head.val >= freq.length || freq[head.val] == false) {
+            current.next = head;
+            current = current.next;
+        }
+        head = head.next;
+    }
+
+    current.next = null;
+    return temp.next;
+};
