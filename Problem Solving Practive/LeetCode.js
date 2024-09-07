fb13c908a0f78f9b95759459db455b8cf9078270
Usman Khan <usman918khan@gmail.com>
2024-09-07 22:24:48 +0500

    leetcode

--- Problem Solving Practive/LeetCode.js
+++ Problem Solving Practive/LeetCode.js
@@ -679,3 +679,30 @@ var modifiedList = function (nums, head) {
     current.next = null;
     return temp.next;
 };
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const res = [];
+
+   function dfs(openP, closeP, s) {
+       if (openP === closeP && openP + closeP === n * 2) {
+           res.push(s);
+           return;
+       }
+
+       if (openP < n) {
+           dfs(openP + 1, closeP, s + "(");
+       }
+
+       if (closeP < openP) {
+           dfs(openP, closeP + 1, s + ")");
+       }
+   }
+
+   dfs(0, 0, "");
+
+   return res;    
+};
